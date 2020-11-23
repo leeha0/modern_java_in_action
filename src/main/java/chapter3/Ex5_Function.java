1357e28493cd18d6bf68aa1a0f2459a3958873df
leeha0 <gkdud2837@gmail.com>
2020-11-23 20:48:52 +0900

    [Chapter03] Function 인터페이스 예시

---
+++ src/main/java/chapter3/Ex5_Function.java
@@ -0,0 +1,22 @@
+package chapter3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class Ex5_Function {
+
+    public static void main(String[] args) {
+        List<Integer> l = map(Arrays.asList("lambdas", "in", "action"), (String s) -> s.length());
+        System.out.println(l.toString());
+    }
+
+    public static <T, R> List<R> map(List<T> list, Function<T, R> f) {
+        List<R> result = new ArrayList<>();
+        for (T t : list) {
+            result.add(f.apply(t));
+        }
+        return result;
+    }
+}
