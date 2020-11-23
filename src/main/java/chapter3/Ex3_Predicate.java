dcb3ea4c08e65e891d2a5b496cc813defc03f4b5
leeha0 <gkdud2837@gmail.com>
2020-11-23 19:53:59 +0900

    [Chatper03] Predicate 예시

---
+++ src/main/java/chapter3/Ex3_Predicate.java
@@ -0,0 +1,30 @@
+package chapter3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Ex3_Predicate {
+
+    public static void main(String[] args) {
+        List<String> listOfStrings = new ArrayList<>();
+        listOfStrings.add("");
+        listOfStrings.add("test1");
+        listOfStrings.add(null);
+
+        Predicate<String> nonEmptyStringPredicate = (String s) -> !s.isEmpty();
+        List<String> nonEmpty = filter(listOfStrings, nonEmptyStringPredicate);
+
+        System.out.println(nonEmpty.size());
+    }
+
+    public static <T> List<T> filter(List<T> list, Predicate<T> p) {
+        List<T> results = new ArrayList<>();
+        for (T t : list) {
+            if (p.test(t)) {
+                results.add(t);
+            }
+        }
+        return results;
+    }
+}
