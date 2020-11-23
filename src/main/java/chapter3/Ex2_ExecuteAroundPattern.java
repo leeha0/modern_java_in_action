8c57112948a4c92f3941a292138305f4b18c661d
leeha0 <gkdud2837@gmail.com>
2020-11-23 18:05:05 +0900

    [Chapter03] 실행 어라운드 패턴 - 동작 파라미터화

--- src/main/java/chapter3/Ex2_ExecuteAroundPattern.java
+++ src/main/java/chapter3/Ex2_ExecuteAroundPattern.java
@@ -1,5 +1,6 @@
 package chapter3;
 
+import chapter3.function.BufferedReaderProcessor;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -7,13 +8,24 @@ import java.io.IOException;
 public class Ex2_ExecuteAroundPattern {
 
     public static void main(String[] args) throws IOException {
-        processFile();
+        // 단일 행을 읽을 수 있는 메서드
+        String result1 = processFile();
+
+        // 동작 파라미터화: 여러 행을 읽을 수 있는 메서드 전달
+        String result2 = processFile((BufferedReader br) -> br.readLine() + br.readLine());
     }
 
     public static String processFile() throws IOException {
         try (BufferedReader br =
             new BufferedReader(new FileReader("data.txt"))) {
             return br.readLine();
+        }
+    }
+
+    public static String processFile(BufferedReaderProcessor processor) throws IOException {
+        try (BufferedReader br =
+            new BufferedReader(new FileReader("data.txt"))) {
+            return processor.process(br);
         }
     }
 }
