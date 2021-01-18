3ce3a61c47d178eec401ac8a561ebfa4cea27f13
leeha0 <gkdud2837@gmail.com>
2021-01-18 17:31:20 +0900

    [Chapter6] Collector 구현하기

--- src/main/java/chapter6/Ex19_toListCollector.java
+++ src/main/java/chapter6/Ex19_toListCollector.java
@@ -1,6 +1,8 @@
 package chapter6;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.BiConsumer;
@@ -13,6 +15,7 @@ public class Ex19_toListCollector<T> implements Collector<T, List<T>, List<T>> {
 
     @Override
     public Supplier<List<T>> supplier() {
+        //
         // 초기 빈 결과 생성
         return ArrayList::new;
     }
@@ -40,6 +43,11 @@ public class Ex19_toListCollector<T> implements Collector<T, List<T>, List<T>> {
 
     @Override
     public Set<Characteristics> characteristics() {
-        return null;
+        return Collections.unmodifiableSet(
+            EnumSet.of(
+                Collector.Characteristics.UNORDERED,
+                Collector.Characteristics.CONCURRENT,
+                Collector.Characteristics.IDENTITY_FINISH
+            ));
     }
 }
